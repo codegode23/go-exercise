ff31fa475d87cd26a20e992ba296aced6feed64c
codegode23 <jasperakabua020@gmail.com>
2023-06-07 14:19:52 +0000

    Excersise 1 done

--- EX1.go
+++ EX1.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+// Write a program which will find all such numbers which are divisible by
+// 7 but are not a multiple of 5, between 2000 and 3200 (both included).
+// The numbers obtained should be printed in a comma-separated sequence on a single line.
+// 1.1. Hint:
+
+// Consider using strconv and strings.Join
 
 func main() {
-	fmt.Println("Gooder")
+
+	for i := 2000; i <= 3200; i++ {
+
+		if i%7 == 0 && i%5 != 0 {
+			g := strconv.Itoa(i)
+			fmt.Printf("%v,", g)
+		}
+
+	}
+
 }
