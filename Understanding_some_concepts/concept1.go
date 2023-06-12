82f22ed0ca0b805fb67e73f2b65ea2052558b3e4
codegode23 <jasperakabua020@gmail.com>
2023-06-12 10:14:28 +0000

    Updated

---
+++ Understanding_some_concepts/concept1.go
@@ -0,0 +1,16 @@
+//Create an array with the number 0 to 10
+//Create an array of strings with names
+
+package main
+
+import "fmt"
+
+func main() {
+	var a = []int64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	var names = []string{"Jasper", "Kojo", "Akabua"}
+
+	b := len(a)
+
+	fmt.Println(b)
+	fmt.Println(names)
+}
