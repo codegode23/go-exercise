cbb242b5efa6a208a9dd4614d2ee146beb93827f
codegode23 <jasperakabua020@gmail.com>
2023-06-12 13:04:35 +0000

    concept1 updated

--- Understanding_some_concepts/concept1.go
+++ Understanding_some_concepts/concept1.go
@@ -1,3 +1,4 @@
+//Create a struct house with variables noRooms, price and city
 //The golang example creates a new struct. Then it sets the variables.
 
 package main
@@ -9,6 +10,12 @@ type Person struct {
 	job  string
 }
 
+type House struct {
+	noRooms bool
+	price   int
+	city    string
+}
+
 func main() {
 	var aperson Person
 
@@ -16,5 +23,16 @@ func main() {
 	aperson.job = "Professor"
 
 	fmt.Printf("aperson.name =  %s\n", aperson.name)
-	fmt.Printf("aperson.job  =  %s\n", aperson.job)
+	fmt.Printf("aperson.job  =  %s\n\n", aperson.job)
+
+	var inDaHouse House
+
+	inDaHouse.noRooms = true
+	inDaHouse.price = 23
+	inDaHouse.city = "Accra"
+
+	fmt.Printf("Information on the house\n")
+	fmt.Printf("Are there available rooms in the house? %v \n", inDaHouse.noRooms)
+	fmt.Printf("The price is Ghc%d\n", inDaHouse.price)
+	fmt.Printf("The house is located in %v \n", inDaHouse.city)
 }
