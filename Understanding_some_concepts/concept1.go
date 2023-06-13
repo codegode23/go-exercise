c0d970c62082dcc1fdb41600dc36440e9fa6f911
codegode23 <jasperakabua020@gmail.com>
2023-06-13 00:52:50 +0000

    New file added as a result of the defer statemen

--- Understanding_some_concepts/concept1.go
+++ Understanding_some_concepts/concept1.go
@@ -1,33 +1,9 @@
-// Make a program that rolls a dice (1 to 6)
-
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
-// func random(min int, max int) int {
-// 	return rand.Intn(max-min) + min
-// }
-
-// func main() {
-
-// 	rand.Seed(time.Now().UnixNano())
-// 	diceRoll := random(1, 6)
-// 	fmt.Printf("Dice roll result: %d\n", diceRoll)
-// }
-
-// Can you generate negative numbers?
-func RandInt(lower, upper int) int {
-	rand.Seed(time.Now().UnixNano())
-	rng := upper - lower
-	return rand.Intn(rng) + lower
-}
+import "fmt"
 
 func main() {
-	randNeg := RandInt(-2, 10)
-
-	fmt.Println(randNeg)
+	defer fmt.Println("Hello")
+	defer fmt.Println("!")
+	fmt.Println("World")
 }
