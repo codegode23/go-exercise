2764a0ab1c7b330b054b0787e3044d52af4f0bc3
codegode23 <jasperakabua020@gmail.com>
2023-06-25 21:56:34 +0000

    go routine example added

--- Understanding_some_concepts/concept1.go
+++ Understanding_some_concepts/concept1.go
@@ -1,20 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
 
-func f(c chan string) {
-	c <- "f() was here"
-}
+func responseSize(url string) {
+	fmt.Println("Step1: ", url)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-func f2(c chan string) {
-	msg := <-c
-	fmt.Println("f2", msg)
+	fmt.Println("Step2: ", url)
+	defer response.Body.Close()
+
+	fmt.Println("Step3: ", url)
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Step4: ", len(body))
 }
 
 func main() {
-	var c chan string = make(chan string)
-	go f(c)
-	go f2(c)
-
-	//fmt.Scanln()
+	go responseSize("https://www.golangprograms.com")
+	go responseSize("https://coderwall.com")
+	go responseSize("https://stackoverflow.com")
+	time.Sleep(10 * time.Second)
 }
