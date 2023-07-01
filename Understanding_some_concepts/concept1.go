89329e4cfe1cb4e77606704d1cbc363c16459505
codegode23 <jasperakabua020@gmail.com>
2023-07-01 09:55:42 +0000

    Go routines attempted again

--- Understanding_some_concepts/concept1.go
+++ Understanding_some_concepts/concept1.go
@@ -1,34 +1,84 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"io/ioutil"
+// 	"log"
+// 	"net/http"
+// 	"time"
+// )
+
+// func responseSize(url string) {
+// 	fmt.Println("Step1: ", url)
+// 	response, err := http.Get(url)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	fmt.Println("Step2: ", url)
+// 	defer response.Body.Close()
+
+// 	fmt.Println("Step3: ", url)
+// 	body, err := ioutil.ReadAll(response.Body)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Println("Step4: ", len(body))
+// }
+
+// func main() {
+// 	go responseSize("https://www.golangprograms.com")
+// 	go responseSize("https://coderwall.com")
+// 	go responseSize("https://stackoverflow.com")
+// 	time.Sleep(10 * time.Second)
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func goroutine1(str string) {
+// 	for w := 0; w < 6; w++ {
+// 		fmt.Println(str)
+// 	}
+// }
+
+// func main() {
+
+// 	//Calling goroutine
+// 	go goroutine1("It is a goroutine!!")
+// 	time.Sleep(1 * time.Second)
+// 	//Calling normal function
+// 	goroutine1("It is a function.")
+// }
+
 package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 	"time"
 )
 
-func responseSize(url string) {
-	fmt.Println("Step1: ", url)
-	response, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
+func number() { //creating a goroutine
+	for i := 1; i <= 5; i++ {
+		time.Sleep(400 * time.Millisecond) //making the goroutine sleep for 400 milliseconds
+		fmt.Printf("%d ", i)
 	}
+}
 
-	fmt.Println("Step2: ", url)
-	defer response.Body.Close()
-
-	fmt.Println("Step3: ", url)
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
+func alphabet() { //creating a goroutine
+	for i := 'a'; i <= 'g'; i++ {
+		time.Sleep(500 * time.Millisecond) //making the goroutine sleep for 500 milliseconds
+		fmt.Printf("%c ", i)
 	}
-	fmt.Println("Step4: ", len(body))
 }
 
 func main() {
-	go responseSize("https://www.golangprograms.com")
-	go responseSize("https://coderwall.com")
-	go responseSize("https://stackoverflow.com")
-	time.Sleep(10 * time.Second)
+	go number()                         //calling a goroutine
+	go alphabet()                       //calling a goroutine
+	time.Sleep(9000 * time.Millisecond) //making the main goroutine sleep for 9000 milliseconds
+	fmt.Println("\nProgram terminated!!")
 }
