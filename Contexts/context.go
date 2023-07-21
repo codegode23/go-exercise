37e7d9df843e9cef94e2894a7483a0343c66c304
codegode23 <jasperakabua020@gmail.com>
2023-07-21 23:04:29 +0000

    Updated ctx variable again

--- Contexts/context.go
+++ Contexts/context.go
@@ -10,7 +10,9 @@ func doSomething(ctx context.Context) {
 }
 
 func main() {
-	//ctx := context.TODO()
-	ctx := context.Background()
+	//ctx := context.TODO() create an empty (or starting) context.
+	//You can use this as a placeholder when you’re not sure which context to use.
+	ctx := context.Background() //creates an empty context like context.TODO does,
+	//but it’s designed to be used where you intend to start a known context.
 	doSomething(ctx)
 }
